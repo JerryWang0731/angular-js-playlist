c4e422e9e0dedee0d0d8f235c3c141b4252b77f4
Jerry <jerry.wang@xuenn.com>
2021-03-02 10:52:05 +0800

    ng-repeat filter

--- app/app.js
+++ app/app.js
@@ -1,5 +1,14 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', ['$scope', function($scope){
-    $scope.greet = "hello due";
+    $scope.search = "";
+
+    $scope.ninjas = [
+        { name: 'Jerry', belt: 'blue', rate: 50, available: true},
+        { name: 'Dasu', belt: 'red', rate: 150, available: true},
+        { name: 'Sici', belt: 'green', rate: 250, available: true},
+        { name: 'Mario', belt: 'yello', rate: 250, available: true},
+        { name: 'Makyo', belt: 'gray', rate: 50, available: false},
+    ]; 
+    
 }]);
