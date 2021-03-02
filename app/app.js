c33dc6f0f5963c2c73d1abcd242f966c73f308fa
Jerry <jerry.wang@xuenn.com>
2021-03-02 11:15:33 +0800

    dynamic order by column

--- app/app.js
+++ app/app.js
@@ -2,13 +2,15 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', ['$scope', function($scope){
     $scope.search = "";
+    $scope.orderBy = "";
 
     $scope.ninjas = [
-        { name: 'Jerry', belt: 'blue', rate: 50, available: true},
+        { name: 'Aerry', belt: 'red', rate: 10, available: true},
+        { name: 'Jerry', belt: 'blue', rate: 10, available: true},
         { name: 'Dasu', belt: 'red', rate: 150, available: true},
         { name: 'Sici', belt: 'green', rate: 250, available: true},
-        { name: 'Mario', belt: 'yello', rate: 250, available: true},
-        { name: 'Makyo', belt: 'gray', rate: 50, available: false},
+        { name: 'Mario', belt: 'yello', rate: 20, available: true},
+        { name: 'Makyo', belt: 'gray', rate: 20, available: false},
     ]; 
     
 }]);
