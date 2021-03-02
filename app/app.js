5e01d9bcabb88aac78c71a1b7d86babd9a22e472
Jerry <jerry.wang@xuenn.com>
2021-03-02 11:40:15 +0800

    ng-click remove from array

--- app/app.js
+++ app/app.js
@@ -4,12 +4,21 @@ myApp.controller('MyController', ['$scope', function($scope){
     $scope.search = "";
     $scope.orderBy = "";
 
+
+    $scope.removeMember = function(member){
+        const rmMemberIdx = $scope.ninjas.indexOf(member);
+
+        if (rmMemberIdx > -1) {
+            $scope.ninjas.splice(rmMemberIdx, 1);
+        }
+    };
+
     $scope.ninjas = [
         { name: 'Aerry', belt: 'red', rate: 10, available: true},
         { name: 'Jerry', belt: 'blue', rate: 10, available: true},
         { name: 'Dasu', belt: 'red', rate: 150, available: true},
         { name: 'Sici', belt: 'green', rate: 250, available: true},
-        { name: 'Mario', belt: 'yello', rate: 20, available: true},
+        { name: 'Mario', belt: 'yellow', rate: 20, available: true},
         { name: 'Makyo', belt: 'gray', rate: 20, available: false},
     ]; 
     
